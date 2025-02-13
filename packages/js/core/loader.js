9757b1c0860caa7a8647872f45ec5334ac386b4a
Dan Knights <danknights95@gmail.com>
2025-02-13 23:48:14 +0000

    refactor(js): move target code into loader file

--- packages/js/core/loader.js
+++ packages/js/core/loader.js
@@ -1,19 +1,45 @@
 #!/usr/bin/env node
 import { spawnSync } from "node:child_process";
+import { createRequire } from "node:module";
+import path from "node:path";
 
-import { getAbsoluteTargetExePath, getTargetTriple } from "./target.js";
+const require = createRequire(import.meta.url);
 
-const targetTriple = getTargetTriple();
-const targetExePath = getAbsoluteTargetExePath(targetTriple);
+const archMap = {
+  x64: "x86_64",
+  arm64: "aarch64",
+};
 
-const result = spawnSync(targetExePath, process.argv.slice(2), {
+const platformMap = {
+  win32: "pc-windows-msvc",
+  darwin: "apple-darwin",
+  linux: "unknown-linux-gnu",
+};
+
+const archName = archMap[process.arch];
+const platformName = platformMap[process.platform];
+
+if (!archName) {
+  throw new Error(`[changenog]: unsupported architecture: ${process.arch}`);
+}
+
+if (!platformName) {
+  throw new Error(`[changenog]: unsupported platform: ${process.platform}`);
+}
+
+const target = `${archName}-${platformName}`;
+const exeName = `changenog${process.platform === "win32" ? ".exe" : ""}`;
+const targetExePath = path.join(`@changenog/${target}`, exeName);
+const absoluteTargetExePath = require.resolve(targetExePath);
+
+const result = spawnSync(absoluteTargetExePath, process.argv.slice(2), {
   stdio: "inherit",
 });
 
 if (result.error) {
   console.error(
-    `[changenog]: failed to execute binary for target: ${targetTriple}.  ensure the` +
-      ` correct optional dependency is installed (@changenog/${targetTriple})\n`,
+    `[changenog]: failed to execute binary for target: ${target}.  ensure the` +
+      ` correct optional dependency is installed (@changenog/${target})\n`,
   );
 
   throw result.error;
