9be8696627352ea996e583180d19aa9166173f03
Dan Knights <danknights95@gmail.com>
2025-02-16 21:39:22 +0000

    test: replace dynamic output values

--- test/output/changelogs/_max_commits_3.md
+++ test/output/changelogs/_max_commits_3.md
@@ -1,5 +1,5 @@
-## [v1.0.0](https://www.my-remote.com/tags) (16/02/2025)
+## [v1.0.0](https://www.my-remote.com/tags) (REPLACED)
 
-- 1.0.0 ([28303a1](https://www.my-remote.com/commit/28303a127c7957f2ce63b20b2493fc1dbc17b5d9))
-- non-angular commit ([fc0bfc7](https://www.my-remote.com/commit/fc0bfc7c39b8a0ae7c22bcc0fccdaf7a5cd16d7f))
-- refactor: refactor code ([a14d37d](https://www.my-remote.com/commit/a14d37d9763be914ac9fc60a6777c0f8c239a1ca))
+- 1.0.0 ([REPLACED](https://www.my-remote.com/commit/REPLACED))
+- non-angular commit ([REPLACED](https://www.my-remote.com/commit/REPLACED))
+- refactor: refactor code ([REPLACED](https://www.my-remote.com/commit/REPLACED))
--- test/utils.ts
+++ test/utils.ts
@@ -12,29 +12,53 @@ export function run(
   });
 }
 
+export function commit(msg: string, dir: "foo" | "bar" | "bar/baz") {
+  fs.appendFileSync(`test/repo/${dir}/main.txt`, msg);
+
+  run("git", ["add", "."]);
+  run("git", ["commit", "-m", msg]);
+}
+
 export function output(args: string[]) {
   const result = run("../../target/release/changenog", args, {
     stdio: "pipe",
   });
 
   fs.appendFileSync("test/output/stdout.txt", `test: ["${args.join(", ")}"]\n`);
-  fs.appendFileSync("test/output/stdout.txt", `${result.stdout ?? result.stderr}\n\n`);
+  fs.appendFileSync(
+    "test/output/stdout.txt",
+    `${replaceDynamicValues(result.stdout.toString())}\n\n`,
+  );
 
   const filename = args.join("_").replace(/[^a-zA-Z0-9_]+/g, "_");
 
   ["/", "foo/", "bar/", "bar/baz/"].forEach((dir) => {
-    if (!fs.existsSync(`test/repo/${dir}CHANGELOG.md`)) return;
+    const sourcePath = `test/repo/${dir}CHANGELOG.md`;
 
-    fs.renameSync(
-      `test/repo/${dir}CHANGELOG.md`,
-      `test/output/changelogs/${dir}${filename}.md`,
+    if (!fs.existsSync(sourcePath)) return;
+
+    fs.writeFileSync(
+      sourcePath,
+      replaceDynamicValues(fs.readFileSync(sourcePath, "utf-8")),
     );
+
+    fs.renameSync(sourcePath, `test/output/changelogs/${dir}${filename}.md`);
   });
 }
 
-export function commit(dir: string, message: string) {
-  fs.appendFileSync(`test/repo/${dir}/main.txt`, message);
+function replaceDynamicValues(content: string) {
+  const shaMatches = content.match(/(?<=\/commit\/)[^\)]+/g) ?? [];
 
-  run("git", ["add", "."]);
-  run("git", ["commit", "-m", message]);
+  return (
+    shaMatches
+      .reduce((acc, sha) => {
+        const shaHead = sha.substring(0, 7);
+        const shaTail = sha.substring(7);
+        const shaRegex = new RegExp(`${shaHead}(${shaTail})?`, "g");
+
+        return acc.replace(shaRegex, "REPLACED");
+      }, content)
+      // Replace no-link shas and dates
+      .replace(/\([^\)]+\)(?=\n)/g, "(REPLACED)")
+  );
 }
